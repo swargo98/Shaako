4824a7a3029e4f71c4aac36cebeaa33fc25c92d7
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-07-16 18:06:28 +0600

    button fixed

--- shaako_fend/src/popup/TriggerButton/index.js
+++ shaako_fend/src/popup/TriggerButton/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 const Trigger = ({ triggerText, buttonRef, showModal }) => {
   return (
     <button
-      className="btn btn-lg btn-danger center modal-button"
       ref={buttonRef}
       onClick={showModal}
+
+      className="btn btn-primary" type="button"
+      style={{background: "rgb(54,247,22)"}}
     >
       {triggerText}
     </button>
