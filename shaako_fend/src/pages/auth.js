6aa9d8b3b711574d295f24abe294d5dab7646cf6
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-07-16 17:50:39 +0600

    popup added

--- shaako_fend/src/pages/auth.js
+++ shaako_fend/src/pages/auth.js
@@ -1,77 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
 
 const Authentication = () => {
-    let [username, setusername] = useState(null)
-    let [password, setpassword] = useState(null)
-    let [isLoggedIn, setisLoggedIn] = useState(false)
-    let [failedLogin, setfailedLogin] = useState(false)
-
-    let handleChangeUsername = (value) => {
-        setusername(username => ({ ...username, 'username': value }))
-    }
-    
-    let handleChangePassword = (value) => {
-        setpassword(password => ({ ...password, 'password': value }))
-    }
-
-    let handleSubmit = async () => {
-        if (username && password) {
-            console.log("Username : ", username, " Password : ", password)
-            let response = await fetch('http://127.0.0.1:8000/organization/login', {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, password })
-            })
-            let data = await response.json()
-            if (data === 'True') {
-                setisLoggedIn(true)
-            }
-            else {
-                setfailedLogin(true)
-            }
-        }
-    }
-
     return (
-        <>
-            <div className="Auth-form-container">
-                <form className="Auth-form">
-                    <div className="Auth-form-content">
-                        <h3 className="Auth-form-title">Sign In</h3>
-                        <div className="form-group mt-3">
-                            <label>Email address</label>
-                            <input
-                                type="email"
-                                className="form-control mt-1"
-                                placeholder="Enter email"
-                                onChange={(e) => { handleChangeUsername(e.target.value) }} value={username?.body}
-                            />
-                        </div>
-                        <div className="form-group mt-3">
-                            <label>Password</label>
-                            <input
-                                type="password"
-                                className="form-control mt-1"
-                                placeholder="Enter password"
-                                onChange={(e) => { handleChangePassword(e.target.value) }} value={password?.body}
-                            />
-                        </div>
-                        <div className="d-grid gap-2 mt-3">
-                            <button type="button" className="btn btn-primary" onClick={handleSubmit}>
-                                Submit
-                            </button>
-                            {failedLogin && <p style={{color: "red"}}>Login Failed!</p>}
-                        </div>
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign In</h3>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Enter email"
+                        />
                     </div>
-                </form>
-            </div>
-            {isLoggedIn && <Navigate to="/home" replace={true} />}
-        </>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="forgot-password text-right mt-2">
+                        Forgot <a href="/">password?</a>
+                    </p>
+                </div>
+            </form>
+        </div>
     );
 }
 
