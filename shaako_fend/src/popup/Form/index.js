d0a6a6c17a10a1a6be2202b38cedf0cad185e577
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-07-16 18:13:24 +0600

    Revert "dropdown added"
    This reverts commit b138b10e1118cb5fda380f64e7b5b41ae7f313b8.

--- shaako_fend/src/popup/Form/index.js
+++ shaako_fend/src/popup/Form/index.js
@@ -5,32 +5,16 @@ export const Form = ({ onSubmit }) => {
     <form onSubmit={onSubmit}>
       <div className="form-group">
         <label htmlFor="name">বিভাগ</label>
-          <br/>
-          <select className="form-control" name="cars" id="cars">
-              <option value="volvo">Volvo</option>
-              <option value="saab">Saab</option>
-              <option value="mercedes">Mercedes</option>
-              <option value="audi">Audi</option>
-          </select>
+        <input className="form-control" id="name" />
       </div>
         <div className="form-group">
             <label htmlFor="name">জেলা</label>
-            <select className="form-control" name="cars" id="cars">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="audi">Audi</option>
-            </select>
+            <input className="form-control" id="name" />
         </div>
 
         <div className="form-group">
             <label htmlFor="name">উপজেলা</label>
-            <select className="form-control" name="cars" id="cars">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="audi">Audi</option>
-            </select>
+            <input className="form-control" id="name" />
         </div>
         <br/>
       <div className="form-group">
