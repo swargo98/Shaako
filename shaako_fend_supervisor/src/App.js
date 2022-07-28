e00c1ed7a00968f9331aadacb7c3b60de8d82960
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-07-28 08:51:09 +0600

    small update

--- shaako_fend_supervisor/src/App.js
+++ shaako_fend_supervisor/src/App.js
@@ -3,9 +3,12 @@ import NavbarSupervisor from "./components/NavbarSupervisor";
 import Footer from "./components/footer";
 import Home from "./pages/home";
 import Authentication from "./pages/auth";
-import RichTextEditor from "./components/richTextEditor";
 import NewLesson from "./pages/newLesson";
+import CHWList from "./pages/chwList";
+import ViewContents from "./pages/viewContents";
+import BlogPost from "./pages/blogPost";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
     return (
@@ -18,8 +21,10 @@ function App() {
                 <Routes>
                     <Route exact path='/home' element={<Home/>} />
                     <Route exact path='/login' element={<Authentication/>} />
-                    <Route exact path='/editor' element={<RichTextEditor/>} />
                     <Route exact path='/new_lesson' element={<NewLesson/>} />
+                    <Route exact path='/chw_list' element={<CHWList/>} />
+                    <Route exact path='/view_contents' element={<ViewContents/>} />
+                    <Route exact path='/blog_post' element={<BlogPost/>} />
                 </Routes>
             </div>
             <Footer />
