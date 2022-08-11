836137da5b3d5c9f4a73ffc52940833eca983899
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-08-12 00:04:12 +0600

    auth done

--- Shaako_App/ShaakoCHW/src/pages/auth.js
+++ Shaako_App/ShaakoCHW/src/pages/auth.js
@@ -9,88 +9,122 @@ import {
   TextInput,
   Button,
   TouchableOpacity,
+  AsyncStorageStatic,
 } from "react-native";
 
+import AsyncStorage from '@react-native-community/async-storage';
+
+
+
 const Authentication = () => {
-    // let [username, setusername] = useState(null)
-    // let [password, setpassword] = useState(null)
-    // let [isLoggedIn, setisLoggedIn] = useState(false)
-    // let [failedLogin, setfailedLogin] = useState(false)
+  let [username, setusername] = useState(null)
+  let [password, setpassword] = useState(null)
+  let [isLoggedIn, setisLoggedIn] = useState(false)
+  let [failedLogin, setfailedLogin] = useState(false)
 
-    // useEffect(() => {
-    //     checkAlreadyLogged()
-    // }, [])
+  useEffect(() => {
+    checkAlreadyLogged()
 
-    // let checkAlreadyLogged = () => {
-    //     let res = localStorage.getItem('logged')
-    //     if (res === 'true') {
-    //         setisLoggedIn(true)
-    //     }
-    // }
+    let _retrieveData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('token');
+        if (value !== null) {
+          // We have data!!
+          console.log(value);
+          navigation.navigate('Home');
+        }
+      } catch (error) {
+        // Error retrieving data
+      }
+    };
 
-    // let handleChangeUsername = (value) => {
-    //     setusername(username => ({ ...username, 'username': value }))
-    // }
+  }, [])
 
-    // let handleChangePassword = (value) => {
-    //     setpassword(password => ({ ...password, 'password': value }))
-    // }
+  let checkAlreadyLogged = () => {
+    
+  }
 
-    // let handleSubmit = async () => {
-    //     if (username && password) {
-    //         console.log("Username : ", username, " Password : ", password)
-    //         let response = await fetch('http://127.0.0.1:8000/organization/login', {
-    //             method: "POST",
-    //             headers: {
-    //                 'Content-Type': 'application/json'
-    //             },
-    //             body: JSON.stringify({ username, password })
-    //         })
-    //         let data = await response.json()
-    //         if (data.correct === 'True') {
-    //             setisLoggedIn(true)
-    //             localStorage.setItem('token', data.token)
-    //             localStorage.setItem('organization', data.organization)
-    //             localStorage.setItem('admin_id', data.id)
-    //             console.log(data.token)
-    //         }
-    //         else {
-    //             setfailedLogin(true)
-    //         }
-    //     }
-    // }
+  let handleChangeUsername = (value) => {
+    console.log(value)
+    setusername(username => ({ ...username, 'username': value }))
+  }
 
-    const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
- 
+  let handleChangePassword = (value) => {
+    console.log(value)
+    setpassword(password => ({ ...password, 'password': value }))
+  }
+
+  let handleSubmit = async () => {
+    if (username && password) {
+      console.log("Username : ", username, " Password : ", password)
+      let response = await fetch('http://127.0.0.1:8000/chw/login', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      })
+      let data = await response.json()
+      if (data.correct === 'True') {
+        setisLoggedIn(true)
+
+        let _storeData = async () => {
+          try {
+            await AsyncStorage.setItem('token', data.token);
+            await AsyncStorage.setItem('organization', data.organization);
+            await AsyncStorage.setItem('chw_id', data.id);
+          } catch (error) {
+            // Error saving data
+            console.log(error)
+          }
+        };
+        // AsyncStorageStatic.setItem('token', data.token)
+        // AsyncStorageStatic.setItem('organization', data.organization)
+        // AsyncStorageStatic.setItem('chw_id', data.id)
+        console.log(data.token)
+      }
+      else {
+        setfailedLogin(true)
+      }
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={require("./../../assets/logo.png")} />
- 
+
       <StatusBar style="auto" />
       <View style={styles.inputView}>
         <TextInput
           style={styles.TextInput}
           placeholder="ইমেইল"
           placeholderTextColor="white"
-          onChangeText={(email) => setEmail(email)}
+          onChangeText={handleChangeUsername}
+          value={username?.body}
         />
       </View>
- 
+
       <View style={styles.inputView}>
         <TextInput
           style={styles.TextInput}
           placeholder="পাসওয়ার্ড"
           placeholderTextColor="white"
           secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
+          onChangeText={handleChangePassword}
+          value = {password?.body}
         />
       </View>
- 
-      <TouchableOpacity style={styles.loginBtn}>
+
+      <TouchableOpacity style={styles.loginBtn} onPress={handleSubmit}>
         <Text style={styles.loginText}>লগ ইন</Text>
       </TouchableOpacity>
+      <View>
+        {failedLogin && <Text style={styles.failedLogin}>লগ ইন ফেইলড</Text>}
+        {isLoggedIn && <Navigate to="/home" replace={true} />}
+      </View>
     </View>
+
   );
 }
 
@@ -101,33 +135,33 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
- 
+
   image: {
     marginBottom: 40,
   },
- 
+
   inputView: {
     backgroundColor: "#7ca4e2",
     borderRadius: 30,
     width: "80%",
     height: 45,
     marginBottom: 20,
- 
+
     alignItems: "center",
   },
- 
+
   TextInput: {
     height: 50,
     flex: 1,
     padding: 10,
     marginLeft: 20,
   },
- 
+
   forgot_button: {
     height: 30,
     marginBottom: 30,
   },
- 
+
   loginBtn: {
     width: "80%",
     borderRadius: 25,
@@ -140,6 +174,10 @@ const styles = StyleSheet.create({
 
   loginText: {
     color: "white"
+  },
+
+  failedLogin: {
+    color: "red",
   }
 });
 
