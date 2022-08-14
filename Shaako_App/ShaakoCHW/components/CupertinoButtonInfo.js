92f6ec7afb9e5dcbd4d50faad27b518dba033c1c
IftekharHakimK <iftekharhakimkaowsar88@gmail.com>
2022-08-14 15:04:00 +0600

    comp

---
+++ Shaako_App/ShaakoCHW/components/CupertinoButtonInfo.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+function CupertinoButtonInfo(props) {
+  return (
+    <TouchableOpacity style={[styles.container, props.style]}>
+      <Text style={styles.buttonHelloWorld} onPress={props.handleClick}>{props.text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgba(120,125,130,1)",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 5,
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  buttonHelloWorld: {
+    color: "#fff",
+    fontSize: 17
+  }
+});
+
+export default CupertinoButtonInfo;
