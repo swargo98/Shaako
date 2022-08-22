20d332854673c16de05c896a63d36879ca0c3b2f
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-08-22 21:49:05 +0600

    symptoms half done

--- Shaako_App/ShaakoCHW/src/pages/symptomsForm.js
+++ Shaako_App/ShaakoCHW/src/pages/symptomsForm.js
@@ -1,74 +1,112 @@
 // import component
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 import MultiSelect from 'react-native-multiple-select';
-import Navbar from '../../components/Navbar';
 
-const items = [{
-    id: '92iijs7yta',
-    name: 'Ondo'
-  }, {
-    id: 'a0s0a8ssbsd',
-    name: 'Ogun'
-  }, {
-    id: '16hbajsabsd',
-    name: 'Calabar'
-  }, {
-    id: 'nahs75a5sg',
-    name: 'Lagos'
-  }, {
-    id: '667atsas',
-    name: 'Maiduguri'
-  }, {
-    id: 'hsyasajs',
-    name: 'Anambra'
-  }, {
-    id: 'djsjudksjd',
-    name: 'Benue'
-  }, {
-    id: 'sdhyaysdj',
-    name: 'Kaduna'
-  }, {
-    id: 'suudydjsjd',
-    name: 'Abuja'
+import { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, AppRegistry, ScrollView } from "react-native";
+import Navbar from "./../../components/Navbar";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+
+
+
+// const items = [{
+//   id: '92iijs7yta',
+//   name: 'Ondo'
+// }, {
+//   id: 'a0s0a8ssbsd',
+//   name: 'Ogun'
+// }, {
+//   id: '16hbajsabsd',
+//   name: 'Calabar'
+// }, {
+//   id: 'nahs75a5sg',
+//   name: 'Lagos'
+// }, {
+//   id: '667atsas',
+//   name: 'Maiduguri'
+// }, {
+//   id: 'hsyasajs',
+//   name: 'Anambra'
+// }, {
+//   id: 'djsjudksjd',
+//   name: 'Benue'
+// }, {
+//   id: 'sdhyaysdj',
+//   name: 'Kaduna'
+// }, {
+//   id: 'suudydjsjd',
+//   name: 'Abuja'
+// }
+// ];
+
+const SymptosForm = ({ navigation }) => {
+
+  let [selectedItems, setselectedItems] = useState([])
+  let [items , setitems] = useState([])
+
+  useEffect(() => {
+    getContents()
+  }, [])
+
+  let getContents = async () => {
+    let sup_id = await AsyncStorage.getItem('sup_id');
+    sup_id = JSON.parse(sup_id)
+
+    let tok = await AsyncStorage.getItem('token')
+    tok = JSON.parse(tok)
+
+    console.log(tok)
+    let response = await fetch(global.ip + '/chw/symptomsList', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'TOKEN ' + tok
+      },
+      body: JSON.stringify(sup_id)
+    })
+    let d = await response.json()
+    // console.log(d)
+    for (let i = 0; i < d.list.length; i++) {
+      let now = d.list[i]
+      let id = i.toString()
+      let name = now
+      setitems(prevArray => [...prevArray, { id: id, name: name }]);
+      // console.log(now)
     }
-];
 
-class SymptosForm extends Component {
-
-  state = {
-    selectedItems : []
+    console.log("etuku")
+    // console.log(items)
+  }
+  let onSelectedItemsChange = (sItems) => {
+    setselectedItems([])
+    for (let i = 0; i < sItems.length; i++) {
+      let id = sItems[i]
+      setselectedItems(prevArray => [...prevArray, id])
+    }
   };
 
-  
-  onSelectedItemsChange = selectedItems => {
-    this.setState({ selectedItems });
-  };
-
-  render() {
-    const { selectedItems } = this.state;
-
-    return (
-      <View style={styles.container}>
+  return (
+    <View style={styles.container}>
       <Navbar></Navbar>
       <View style={{ alignItems: 'center' }}>
-                            <Text style={styles.title}>Symptoms Form</Text>
-                        </View>
-      <View style={{margin: 10}}>
-            <Text style={styles.text}>   Patient Name: </Text>
-            <Text style={styles.text}>   Date: </Text>
-        </View>
-        <View>
+        <Text style={styles.title}>Symptoms Form</Text>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Text style={styles.text}>   Patient Name: </Text>
+        <Text style={styles.text}>   Date: </Text>
+      </View>
+      <View>
         <MultiSelect
           hideTags
           items={items}
           uniqueKey="id"
           ref={(component) => { this.multiSelect = component }}
-          onSelectedItemsChange={this.onSelectedItemsChange}
+          onSelectedItemsChange={onSelectedItemsChange}
           selectedItems={selectedItems}
           selectText=" লক্ষণ সিলেক্ট করুন"
           searchInputPlaceholderText="লক্ষণ খুঁজুন"
-          onChangeInput={ (text)=> console.log(text)}
+          onChangeInput={(text) => console.log(text)}
           tagRemoveIconColor="#CCC"
           tagBorderColor="#CCC"
           tagTextColor="#CCC"
@@ -76,59 +114,59 @@ class SymptosForm extends Component {
           selectedItemIconColor="#CCC"
           itemTextColor="#000"
           displayKey="name"
-          searchInputStyle={{ color: "#000"}}
+          searchInputStyle={{ color: "#000" }}
           styleMainWrapper={{ padding: 10 }}
-          tagContainerStyle={{ backgroundColor:"#000" }}
-          submitButtonColor="#CCC"
-          submitButtonText="Submit"
+          tagContainerStyle={{ backgroundColor: "#000" }}
+          hideSubmitButton = {true}
+          styleListContainer={{height: 256}}
         />
         <View style={{ flex: 1, margin: 10 }}>
           {this.multiSelect && this.multiSelect.getSelectedItemsExt(selectedItems)}
         </View>
-        </View>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        backgroundColor: "#41cca6"
-    },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#41cca6"
+  },
 
-    posts: {
-        flex: 1,
-        flexDirection: "row",
-        alignItems: 'center',
-        padding: 10,
-    },
+  posts: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: 'center',
+    padding: 10,
+  },
 
-    cardContainer: {
-        flex: 1,
-        flexDirection: "row",
-        backgroundColor: "white",
-        padding: 0,
-        alignItems: "center",
-        justifyContent: 'space-between'
-    },
+  cardContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "white",
+    padding: 0,
+    alignItems: "center",
+    justifyContent: 'space-between'
+  },
 
-    title: {
-        fontWeight: "bold",
-        marginVertical: 4,
-        fontSize: 20,
-    },
+  title: {
+    fontWeight: "bold",
+    marginVertical: 4,
+    fontSize: 20,
+  },
 
-    text: {
-        fontWeight: "bold",
-        backgroundColor : "#2089dc",
-        color : "white",
-        // textAlign : "center",
-        paddingVertical : 5,
-        marginBottom : 10,
-        borderRadius: 10
-    }
+  text: {
+    fontWeight: "bold",
+    backgroundColor: "#2089dc",
+    color: "white",
+    // textAlign : "center",
+    paddingVertical: 5,
+    marginBottom: 10,
+    borderRadius: 10
+  }
 
 });
 
