265c8a31f919ca67d978c21f7af1d36aa8e4adc1
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2022-08-25 03:34:16 +0600

    symptomsForm Completed

--- ShaakoProject/Organization/urlsSwargo.py
+++ ShaakoProject/Organization/urlsSwargo.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('organization/profile', viewsSwargo.getProfile),
     path('organization/profileUpdate', viewsSwargo.updateProfile),
     path('chw/symptomsList', viewsSwargo.getSymptomsList),
+    path('chw/getPrediction', viewsSwargo.getPrediction),
+    path('chw/saveVisitForm', viewsSwargo.addVisitForm),
 
 ]
