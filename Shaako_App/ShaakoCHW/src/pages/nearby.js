78998ab2d2d0f00e7af12d5a6e5c18e5dd1d749e
IftekharHakimK <iftekharhakimkaowsar88@gmail.com>
2022-08-27 19:20:37 +0600

    nearby service

--- Shaako_App/ShaakoCHW/src/pages/nearby.js
+++ Shaako_App/ShaakoCHW/src/pages/nearby.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LoginScreen from "react-native-login-screen";
-import { View, Text, StyleSheet, Button, Image, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Button, Image, Dimensions, TouchableOpacity } from "react-native";
+import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
 //import { NativeRouter ,Link } from "react-router-native";
 // import MaterialButtonViolet from "../../components/MaterialButtonViolet";
 import Navbar from "../../components/Navbar";
@@ -14,15 +15,18 @@ import '../../global.js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import Marker from 'react-native-maps'
-
+import { Card, ListItem, Icon, Header } from 'react-native-elements';
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
+import { render } from "react-dom";
 
 export default function Nearby({ navigation }) {
-    let [latitude, setlatitude] = useState('23.7256564');
-    let [longitude, setlongitude] = useState('90.3915638');
+    let [latitude, setlatitude] = useState(23.7256564);
+    let [longitude, setlongitude] = useState(90.3915638);
     let [dummy, setdummy] = useState(false);
-    
+    let [details, setdetails] = useState('নিকটবর্তী হাসপাতাল খুঁজুন');
+    let [show, setshow] = useState(false);
+
     useEffect(() => {
         getData()
     }, [])
@@ -32,71 +36,305 @@ export default function Nearby({ navigation }) {
         console.log(location.coords.latitude + " " + location.coords.longitude)
         // Location.
         setlatitude(location.coords.latitude)
-        setlongitude(location.coords.longitude) 
+        setlongitude(location.coords.longitude)
         setdummy(true)
     }
-    
+
+    mapstyle = [
+        {
+            "featureType": "administrative",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "stylers": [
+                {
+                    "visibility": "off"
+                },
+                {
+                    "weight": 1
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.attraction",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.government",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "stylers": [
+                {
+                    "color": "#f20202"
+                },
+                {
+                    "weight": 5.5
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#78c487"
+                },
+                {
+                    "weight": 1
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "color": "#78c487"
+                },
+                {
+                    "weight": 7.5
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                },
+                {
+                    "weight": 5.5
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#f20202"
+                },
+                {
+                    "visibility": "simplified"
+                },
+                {
+                    "weight": 8
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#eb0f0f"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.place_of_worship",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.school",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.sports_complex",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ]
+    let clicked = async (e) => {
+        console.log(e.nativeEvent)
+        let lat = e.nativeEvent.coordinate.latitude
+        let long = e.nativeEvent.coordinate.longitude
+        let nam = e.nativeEvent.name
+        console.log(nam)
+        setlatitude(lat)
+        setlongitude(long)
+
+        let response = await fetch('https://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + long + '&zoom=18&addressdetails=1')
+        console.log('https://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + long + '&zoom=18&addressdetails=0')
+        let data = await response.json()
+        console.log(data)
+        // console.log(data.display_name)
+
+        nam = nam.trim()
+        while (nam.charAt(nam.length - 1) == '…') {
+            nam = nam.substr(0, nam.length - 1);
+        }
+        nam = nam.trim()
+        while (nam.charAt(nam.length - 1) == '…') {
+            nam = nam.substr(0, nam.length - 1);
+        }
+        let str=data.display_name
+        var arr = str.split(',')
+        arr.splice(0, 1);
+        str=arr.join(',')
+
+        // nam=nam.replace(/(\r\n|\n|\r)/gm, " ")
+        let address = data.address
+
+        let s = nam + ', ' + str
+        setdetails(s)
+        setshow(true)
+    }
+
+
     return (
         <View style={styles.container}>
             <Navbar navigation={navigation}></Navbar>
 
             <MapView
                 style={styles.map}
-                initialCamera={{
-                    center: { latitude: latitude, longitude: longitude },
-                    pitch: 0,
-                    zoom: 18,
-                    heading: 0,
-                    altitude: 1000,
+                // initialCamera={{
+                //     center: { latitude: latitude, longitude: longitude },
+                //     pitch: 0,
+                //     zoom: 18,
+                //     heading: 0,
+                //     altitude: 1000,
+                // }}
+                region={{
+                    latitude: latitude,
+                    longitude: longitude,
+                    latitudeDelta: 0.00122,
+                    longitudeDelta: 0.0121,
+                    zoom: 20,
                 }}
-                provider = { PROVIDER_GOOGLE }
+
+                customMapStyle={mapstyle}
+                // provider = { PROVIDER_GOOGLE }
                 showsUserLocation={true}
                 showsMyLocationButton={true}
-                showsTraffic={true}
-                mapType="terrain"
-                showsBuildings={false}
-                showsIndoors={false}
-                // annotations={markers}
-                loadingEnabled={true}
-                // customMapStyle={mapStyle}
+                onPoiClick={(e) => clicked(e)}
             />
+            {show &&
+                <View style={styles.cardContainer}>
+                    <TouchableOpacity style={styles.iconButton}>
+                        <MaterialCommunityIconsIcon
+                            name="close"
+                            size={40}
+                            alignItems="center"
+                            onPress={() => setshow(false)}
+                        ></MaterialCommunityIconsIcon>
+                    </TouchableOpacity>
+                    <Card.Title style={styles.rect}>{details}</Card.Title>
+                </View>
+            }
+
 
 
         </View>
     );
 }
-var mapStyle = [
-    {
-      "featureType": "poi.business",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "labels.text",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    }
-  ]
 
 const styles = StyleSheet.create({
     container: {
         flex: 1
     },
     rect: {
-        top: 0,
-        left: 0,
         width: "100%",
-        height: 232,
-        position: "absolute",
-        backgroundColor: "rgba(112,245,177,1)",
-        flexDirection: "row"
+        height: 125,
+        // position: "absolute",
+        backgroundColor: "rgba(206, 222, 210,0.8)",
+        flexDirection: "row",
+        marginBottom: 0
     },
     materialButtonViolet1: {
         height: 127,
@@ -135,6 +373,12 @@ const styles = StyleSheet.create({
     map: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
+    },
+    cardContainer: {
+        marginTop: -200,
+    },
+    iconButton: {
+        marginLeft: Dimensions.get('window').width - 40
     },
 });
 
